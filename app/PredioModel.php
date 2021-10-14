fb148e6256b9128891eee56c5737bc0597a2e825
AdrianDiaz99 <jesusadriandiazorozco@gmail.com>
2021-10-14 11:47:37 -0600

    Agregando una sola salida en la funcion agregarPredio()

--- app/PredioModel.php
+++ app/PredioModel.php
@@ -4,48 +4,46 @@ namespace App;
 
 use Exception;
 use Illuminate\Database\Eloquent\Model;
+
 class PredioModel extends Model
 {
     protected $table = 'predios';
     protected $fillable = ['factorLluvia', 'factorHumedad', 'factorResequedad', 'hectareas', 'user_id'];
     protected $keyType = 'string';
+    public $incrementing = false;
 
-    public function getPrediosParaCrud(){
+    public function getPrediosParaCrud()
+    {
         return parent::get(['id', 'FactorLluvia', 'FactorHumedad', 'FactorResequedad', 'Hectareas']);
     }
 
-    public function agregarPredio(Predio $predio){
+    public function agregarPredio(Predio $predio)
+    {
 
         $this->factorLluvia     = $predio->getFactorLluvia();
         $this->factorHumedad    = $predio->getFactorHumedad();
         $this->factorResequedad = $predio->getFactorResequedad();
         $this->hectareas        = $predio->getHectareas();
         $this->user_id          = $predio->getUserAlta();
-        
-        try
-        {
+
+        try {
 
             parent::saveOrFail();
-            
+
             return json_encode(
-                Array(
+                array(
                     "tipo" => "message",
                     "mensaje" => "Predio insertado correctamente"
                 )
             );
-
-        }
-        catch(Exception $e)
-        {
+        } catch (Exception $e) {
 
             return json_encode(
-                Array(
+                array(
                     "tipo" => "error",
                     "mensaje" => "Error al insertar predio"
                 )
             );
-
         }
-
     }
 }
